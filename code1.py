3d42dee5cf0015d167a7feefc948456f5cfb6bb0
JH Kim <brian4924@gmail.com>
2023-01-09 16:52:58 +0900

    iris_data.csv 생성

---
+++ code1.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
+Iris = load_iris()
+
+Iris_Data = pd.DataFrame(data=np.c_[Iris['data'], Iris['target']], columns=Iris['feature_names'] + ['target'])
+Iris_Data['target'] = Iris_Data['target'].map({0: "setosa", 1: "versicolor", 2: "virginica"})
+
+X_Data = Iris_Data.iloc[:, :-1]
+Y_Data = Iris_Data.iloc[:, [-1]]
+
+Iris_Data.to_csv("iris_data.csv")
